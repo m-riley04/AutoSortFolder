0abd576581ae077dd2b970c4da85e3da6ada5aab
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-05-02 01:10:35 -0500

    Update Form1.Designer.cs

--- C#/AutoSortFolder/Form1.Designer.cs
+++ C#/AutoSortFolder/Form1.Designer.cs
@@ -36,19 +36,26 @@
             this.button_start = new System.Windows.Forms.Button();
             this.button_stop = new System.Windows.Forms.Button();
             this.button_unsort = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.combobox_sortingMethod = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // textbox_folderDirectory
             // 
-            this.textbox_folderDirectory.Location = new System.Drawing.Point(150, 49);
+            this.textbox_folderDirectory.Location = new System.Drawing.Point(126, 8);
             this.textbox_folderDirectory.Name = "textbox_folderDirectory";
+            this.textbox_folderDirectory.ReadOnly = true;
             this.textbox_folderDirectory.Size = new System.Drawing.Size(275, 20);
             this.textbox_folderDirectory.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 52);
+            this.label1.Location = new System.Drawing.Point(3, 11);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(117, 13);
             this.label1.TabIndex = 2;
@@ -56,7 +63,7 @@
             // 
             // button_selectFolder
             // 
-            this.button_selectFolder.Location = new System.Drawing.Point(432, 45);
+            this.button_selectFolder.Location = new System.Drawing.Point(407, 7);
             this.button_selectFolder.Name = "button_selectFolder";
             this.button_selectFolder.Size = new System.Drawing.Size(103, 23);
             this.button_selectFolder.TabIndex = 3;
@@ -67,7 +74,7 @@
             // label_status
             // 
             this.label_status.AutoSize = true;
-            this.label_status.Location = new System.Drawing.Point(27, 507);
+            this.label_status.Location = new System.Drawing.Point(591, 473);
             this.label_status.Name = "label_status";
             this.label_status.Size = new System.Drawing.Size(23, 13);
             this.label_status.TabIndex = 4;
@@ -75,7 +82,7 @@
             // 
             // button_start
             // 
-            this.button_start.Location = new System.Drawing.Point(30, 79);
+            this.button_start.Location = new System.Drawing.Point(3, 3);
             this.button_start.Name = "button_start";
             this.button_start.Size = new System.Drawing.Size(59, 23);
             this.button_start.TabIndex = 5;
@@ -85,7 +92,8 @@
             // 
             // button_stop
             // 
-            this.button_stop.Location = new System.Drawing.Point(95, 79);
+            this.button_stop.Enabled = false;
+            this.button_stop.Location = new System.Drawing.Point(68, 3);
             this.button_stop.Name = "button_stop";
             this.button_stop.Size = new System.Drawing.Size(59, 23);
             this.button_stop.TabIndex = 6;
@@ -95,7 +103,7 @@
             // 
             // button_unsort
             // 
-            this.button_unsort.Location = new System.Drawing.Point(160, 79);
+            this.button_unsort.Location = new System.Drawing.Point(133, 3);
             this.button_unsort.Name = "button_unsort";
             this.button_unsort.Size = new System.Drawing.Size(59, 23);
             this.button_unsort.TabIndex = 7;
@@ -103,22 +111,68 @@
             this.button_unsort.UseVisualStyleBackColor = true;
             this.button_unsort.Click += new System.EventHandler(this.button_unsort_Click);
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button_start);
+            this.panel1.Controls.Add(this.button_unsort);
+            this.panel1.Controls.Add(this.button_stop);
+            this.panel1.Location = new System.Drawing.Point(591, 489);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(197, 31);
+            this.panel1.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.textbox_folderDirectory);
+            this.panel2.Controls.Add(this.button_selectFolder);
+            this.panel2.Location = new System.Drawing.Point(12, 482);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(517, 38);
+            this.panel2.TabIndex = 9;
+            // 
+            // combobox_sortingMethod
+            // 
+            this.combobox_sortingMethod.FormattingEnabled = true;
+            this.combobox_sortingMethod.Items.AddRange(new object[] {
+            "Alphabetical",
+            "Date Created",
+            "Date Modified",
+            "Extension",
+            "None"});
+            this.combobox_sortingMethod.Location = new System.Drawing.Point(12, 400);
+            this.combobox_sortingMethod.Name = "combobox_sortingMethod";
+            this.combobox_sortingMethod.Size = new System.Drawing.Size(315, 21);
+            this.combobox_sortingMethod.Sorted = true;
+            this.combobox_sortingMethod.TabIndex = 10;
+            this.combobox_sortingMethod.SelectedIndexChanged += new System.EventHandler(this.combobox_sortingMethod_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 384);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sorting Method";
+            // 
             // Window
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 542);
-            this.Controls.Add(this.button_unsort);
-            this.Controls.Add(this.button_stop);
-            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.combobox_sortingMethod);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label_status);
-            this.Controls.Add(this.button_selectFolder);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textbox_folderDirectory);
             this.Name = "Window";
             this.Text = "AutoSortFolder";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,6 +188,10 @@
         private System.Windows.Forms.Button button_start;
         private System.Windows.Forms.Button button_stop;
         private System.Windows.Forms.Button button_unsort;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox combobox_sortingMethod;
+        private System.Windows.Forms.Label label2;
     }
 }
 
