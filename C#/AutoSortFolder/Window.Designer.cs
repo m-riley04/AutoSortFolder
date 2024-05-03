0416a37ffeb86def27d66a5cfc820e52f79e7fdf
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-05-03 01:08:56 -0500

    Update Window.Designer.cs

--- C#/AutoSortFolder/Window.Designer.cs
+++ C#/AutoSortFolder/Window.Designer.cs
@@ -248,7 +248,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 7);
+            this.label3.Location = new System.Drawing.Point(3, 5);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(73, 13);
             this.label3.TabIndex = 17;
