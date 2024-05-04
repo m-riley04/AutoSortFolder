ae8526cd12da8ba0783c166f253e31bdfd478799
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-05-04 16:52:09 -0500

    move all functionality to seperate methods

--- C#/AutoSortFolder/Window.cs
+++ C#/AutoSortFolder/Window.cs
@@ -33,6 +33,164 @@ namespace AutoSortFolder
             UpdateUI();
         }
 
+        #region High-Level Functionality Methods
+        private void SaveAnchors()
+        {
+            try
+            {
+                app.SaveAnchors();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "Error");
+            }
+        }
+
+        private void LoadAnchors()
+        {
+            try
+            {
+                app.LoadAnchors();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "Error");
+            }
+
+            PopulateAnchors();
+            UpdateUI();
+        }
+
+        private void StartAnchorSorting()
+        {
+            try
+            {
+                if (!sorterWorker.IsBusy)
+                {
+                    // Start the asynchronous operation.
+                    sorterWorker.RunWorkerAsync();
+                    app.ActivateCurrentAnchor();
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "Error");
+            }
+
+            UpdateUI();
+        }
+
+        private void StopAnchorSorting()
+        {
+            try
+            {
+                if (sorterWorker.WorkerSupportsCancellation)
+                {
+                    // Cancel the asynchronous operation.
+                    sorterWorker.CancelAsync();
+                    app.DeactivateCurrentAnchor();
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "Error");
+            }
+
+            UpdateUI();
+        }
+
+        private void SelectAnchorFolder()
+        {
+            try
+            {
+                // Check if the anchor is set
+                if (app.currentAnchor == null)
+                {
+                    app.currentAnchor = app.anchors[0];
+                }
+
+                if (app.currentAnchor.status != AnchorStatus.IDLE) throw new Exception("Cannot change anchor point folder while sorting is in progress");
+
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    app.currentAnchor.directory = folderBrowserDialog.SelectedPath;
+                    UpdateUI();
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "Error");
+            }
+        }
+
+        private void UnsortAnchor()
+        {
+            try
+            {
+                app.currentAnchor.Unsort(progress => { });
+                app.currentAnchor.sorted = false;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show($"Error: {err.Message}");
+            }
+
+            PopulateCurrentAnchorTree();
+            UpdateCurrentAnchorUI();
+        }
+
+        private void RemoveAnchor()
+        {
+            try
+            {
+                int index = listbox_anchors.SelectedIndex;
+                if (index != -1)
+                {
+                    app.anchors.RemoveAt(index);
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "Error");
+            }
+
+            PopulateAnchors();
+            UpdateUI();
+        }
+
+        private void AddAnchor()
+        {
+            try
+            {
+                Anchor newAnchor = new Anchor(listbox_anchors.Items.Count + 1, "...", SortingMethod.NONE);
+                app.anchors.Add(newAnchor);
+                app.currentAnchor = newAnchor;
+                listbox_anchors.Items.Add((listbox_anchors.Items.Count + 1) + ") " + newAnchor.directory);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "Error");
+            }
+
+            this.UpdateUI();
+        }
+        
+        private void OpenURLInBrowser(string url)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(url);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        #endregion
+
+        #region UI Update Methods
+
         private void PopulateCurrentAnchorTree()
         {
             if (app.currentAnchor == null) return;
@@ -140,6 +298,7 @@ namespace AutoSortFolder
             PopulateCurrentAnchorTree();
 
         }
+        
         private void ResetUI()
         {
             // Update Labels
@@ -175,141 +334,47 @@ namespace AutoSortFolder
             }
         }
 
+        #endregion
+
+        #region Button Methods
         private void buttonStart_Click(object sender, EventArgs e)
         {
-            try
-            {
-                if (!sorterWorker.IsBusy)
-                {
-                    // Start the asynchronous operation.
-                    sorterWorker.RunWorkerAsync();
-                    app.ActivateCurrentAnchor();
-                }
-            } catch (Exception err)
-            {
-                MessageBox.Show(err.Message, "Error");
-            }
-
-            UpdateUI();
+            StartAnchorSorting();
         }
 
         private void buttonStop_Click(object sender, EventArgs e)
         {
-            try
-            {
-                if (sorterWorker.WorkerSupportsCancellation)
-                {
-                    // Cancel the asynchronous operation.
-                    sorterWorker.CancelAsync();
-                    app.DeactivateCurrentAnchor();
-                }
-            } catch (Exception err)
-            {
-                MessageBox.Show(err.Message, "Error");
-            }
-
-            UpdateUI();
+            StopAnchorSorting();
         }
 
         private void buttonSelectFolder_Click(object sender, EventArgs e)
         {
-            try
-            {
-                // Check if the anchor is set
-                if (app.currentAnchor == null)
-                {
-                    app.currentAnchor = app.anchors[0];
-                }
-
-                if (app.currentAnchor.status != AnchorStatus.IDLE) throw new Exception("Cannot change anchor point folder while sorting is in progress");
-
-                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
-                {
-                    app.currentAnchor.directory = folderBrowserDialog.SelectedPath;
-                    UpdateUI();
-                }
-            } catch (Exception err)
-            {
-                MessageBox.Show(err.Message, "Error");
-            }
+            SelectAnchorFolder();
         }
 
         private void buttonUnsort_Click(object sender, EventArgs e)
         {
-            try
-            {
-                app.currentAnchor.Unsort(progress => {});
-                app.currentAnchor.sorted = false;
-            } catch (Exception err)
-            {
-                MessageBox.Show(err.Message, "Error");
-            }
-
-            PopulateCurrentAnchorTree();
-            UpdateCurrentAnchorUI();
+            UnsortAnchor();
         }
 
         private void buttonSave_Click(object sender, EventArgs e)
         {
-            try
-            {
-                app.SaveAnchors();
-            }
-            catch (Exception err)
-            {
-                MessageBox.Show(err.Message, "Error");
-            }
+            SaveAnchors();
         }
 
         private void buttonLoad_Click(object sender, EventArgs e)
         {
-            try
-            {
-                app.LoadAnchors();
-            }
-            catch (Exception err)
-            {
-                MessageBox.Show(err.Message, "Error");
-            }
-
-            PopulateAnchors();
-            UpdateUI();
+            LoadAnchors();
         }
 
         private void buttonAdd_Click(object sender, EventArgs e)
         {
-            try
-            {
-                Anchor newAnchor = new Anchor(listbox_anchors.Items.Count + 1, "...", SortingMethod.NONE);
-                app.anchors.Add(newAnchor);
-                app.currentAnchor = newAnchor;
-                listbox_anchors.Items.Add((listbox_anchors.Items.Count + 1) + ") " + newAnchor.directory);
-            }
-            catch (Exception err)
-            {
-                MessageBox.Show(err.Message, "Error");
-            }
-
-            this.UpdateUI();
+            AddAnchor();
         }
 
         private void buttonRemove_Click(object sender, EventArgs e)
         {
-            try
-            {
-                int index = listbox_anchors.SelectedIndex;
-                if (index != -1)
-                {
-                    app.anchors.RemoveAt(index);
-                }
-            }
-            catch (Exception err)
-            {
-                MessageBox.Show(err.Message, "Error");
-            }
-
-            PopulateAnchors();
-            UpdateUI();
+            RemoveAnchor();
         }
 
         private void buttonSelect_Click(object sender, EventArgs e)
@@ -325,6 +390,9 @@ namespace AutoSortFolder
             UpdateCurrentAnchorUI();
         }
 
+        #endregion
+
+        #region Other Component Methods
         private void dropdownSortingMethod_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (app.currentAnchor != null) app.currentAnchor.method = (SortingMethod)combobox_sortingMethod.SelectedIndex;
@@ -341,7 +409,9 @@ namespace AutoSortFolder
                 MessageBox.Show(err.Message, "Error");
             }
         }
+        #endregion
 
+        #region Background Worker Methods
         private void sorterWorker_DoWork(object sender, DoWorkEventArgs e)
         {
             BackgroundWorker worker = sender as BackgroundWorker;
