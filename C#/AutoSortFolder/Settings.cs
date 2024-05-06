1849a79a61670f6256d9dbd0fb3cb1c17337daf7
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-05-05 20:33:11 -0500

    Update Settings.cs

--- C#/AutoSortFolder/Settings.cs
+++ C#/AutoSortFolder/Settings.cs
@@ -9,23 +9,21 @@ namespace AutoSortFolder
     public class Settings
     {
         public bool liveSorting;
-        public bool autoSaving;
+        public bool autoSave;
         public bool autorun;
 
         public Settings()
         {
             this.liveSorting = true;
-            this.autoSaving = true;
+            this.autoSave = true;
             this.autorun = false;
         }
 
         public Settings(bool liveSorting, bool autoSaving, bool autorun)
         {
             this.liveSorting = liveSorting;
-            this.autoSaving = autoSaving;
+            this.autoSave = autoSaving;
             this.autorun = autorun;
         }
-
-
     }
 }
