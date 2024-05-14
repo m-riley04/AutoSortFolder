0b7a692d0e52c1cfa1bf4686f931f43d6cd29fb3
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-05-14 16:30:28 -0500

    Create UpdateHelper.cs

---
+++ C#/AutoSortFolder/UpdateHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSortFolder
+{
+    public class UpdateHelper
+    {
+        string githubURL = "https://github.com/m-riley04/AutoSortFolder";
+        string latestReleaseURL = "https://api.github.com/repos/m-riley04/AutoSortFolder/releases/latest";
+        string version;
+
+        public UpdateHelper() 
+        {
+            version = "0.0.0";
+        }
+
+        public UpdateHelper(string version)
+        {
+            this.version = version;
+        }
+
+        public bool CheckForUpdate()
+        {
+
+
+            return false;
+        }
+    }
+}
