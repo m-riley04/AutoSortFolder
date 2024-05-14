84e1a60b685d8678bb6de8b9a621284a3062f277
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-05-13 23:48:55 -0500

    add debug bool setting

--- C#/AutoSortFolder/Settings.cs
+++ C#/AutoSortFolder/Settings.cs
@@ -11,19 +11,22 @@ namespace AutoSortFolder
         public bool backgroundSorting;
         public bool autoSave;
         public bool autorun;
+        public bool debug;
 
         public Settings()
         {
             this.backgroundSorting = true;
             this.autoSave = true;
             this.autorun = false;
+            this.debug = false;
         }
 
-        public Settings(bool liveSorting, bool autoSaving, bool autorun)
+        public Settings(bool liveSorting, bool autoSaving, bool autorun, bool debug)
         {
             this.backgroundSorting = liveSorting;
             this.autoSave = autoSaving;
             this.autorun = autorun;
+            this.debug = debug;
         }
     }
 }
